c70126e37839b757158442958453013b20eb999b
Musrat J <60440730+Musrat-J-Rimu@users.noreply.github.com>
2024-03-12 10:40:38 +1100

    User Authentication and Authorization
    Amplify integration with FE.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "xmopsaccelerate3d029696": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
