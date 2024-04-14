c5e3c79286cdb09d2b67a4d2d6e82aa353b66c4e
ramanturna <157999191+ramanturna@users.noreply.github.com>
2024-04-15 08:56:31 +1000

    Monolith terraform files

---
+++ terraform/monolith/network.tf
@@ -0,0 +1,43 @@
+resource "aws_vpc" "app_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "MonolithVPC"
+  }
+}
+
+resource "aws_subnet" "app_subnet" {
+  vpc_id                  = aws_vpc.app_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "MonolithSubnet"
+  }
+}
+
+resource "aws_internet_gateway" "app_gw" {
+  vpc_id = aws_vpc.app_vpc.id
+  tags = {
+    Name = "MonolithIGW"
+  }
+}
+
+resource "aws_route_table" "app_rt" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app_gw.id
+  }
+
+  tags = {
+    Name = "MonolithRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.app_subnet.id
+  route_table_id = aws_route_table.app_rt.id
+}
